657736e3ee3191b183890925c9c5928f0fe2b7a9
Kristijan Bartol <kristijan.bartol%40gmail.com>
2024-01-31 15:43:24 +0000

    Preparation to create a 3D grid on the subdivided mesh.

---
+++ grid_3d.py
@@ -0,0 +1,24 @@
+from smplx import SMPL
+import argparse
+
+from const import (
+    PANT_LENGTH,
+    SHIRT_LENGTH,
+    SLEEVE_LENGTH
+)
+from selector import select_subdivided
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gender', '-G', type=str, choices=['male', 'female', 'neutral'], default='female')
+    parser.add_argument('--subdivide', dest='subdivide', action='store_true')
+    parser.add_argument('--shirt_length', '-S', type=float, default=SHIRT_LENGTH)
+    parser.add_argument('--pant_length', '-P', type=float, default=PANT_LENGTH)
+    parser.add_argument('--sleeve_length', '-L', type=float, default=SLEEVE_LENGTH)
+    args = parser.parse_args()
+
+    smpl_model = SMPL(model_path='/data/hierprob3d/smpl/SMPL_FEMALE.pkl')
+
+    verts, faces, boundary_idxs = select_subdivided(args, smpl_model)['upper_front']
+    starting_idx = boundary_idxs[0]
