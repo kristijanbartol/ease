4bbd058142264c48bc98660be21072af27e2cf73
Kristijan Bartol <kristijan.bartol@gmail.com>
2024-10-03 14:52:23 +0200

    Run all the steps in the main module (design, parameterization, pattern visualization).

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from shutil import rmtree
 import json
+import subprocess
 
 from src.dress_processing import (
     setup_directories,
@@ -27,13 +28,14 @@ from src.garment_processing import (
 from src.seams import (
     determine_all_seams
 )
+from src.vis import visualize_pattern
 
 
-def select_skirtified(args, smpl_dir):
+def select_skirtified(args):
     original_dir, skirtified_dir = setup_directories(args)
     set_dict = load_set_dict(args)
 
-    original_meshes, smpl_models = generate_original_meshes(smpl_dir, original_dir, set_dict)
+    original_meshes, smpl_models = generate_original_meshes(args.smpl_dir, original_dir, set_dict)
 
     if not os.path.exists(skirtified_dir):
         print('NOTE: Skirtified meshes not yet created.')
@@ -55,13 +57,13 @@ def select_skirtified(args, smpl_dir):
     garment.store_seamline_vertex_pairs(subdir=f'{args.design}-{args.body_set}')
 
 
-def select_default(args, smpl_dir):
-    smpl_models = initialize_smpl_models(smpl_dir)
+def select_default(args):
+    smpl_models = initialize_smpl_models(args.smpl_dir)
     garment, design_dict, set_dict = initialize_garment_and_configs(args, smpl_models)
     
     seams_info = determine_all_seams(garment, design_dict)
     modified_verts, updated_seams_info = modify_body_mesh(garment, seams_info)
-    modified_models = initialize_modified_smpl_models(smpl_dir, modified_verts)
+    modified_models = initialize_modified_smpl_models(args.smpl_dir, modified_verts)
     
     garment_parts = flood_fill_all_parts(garment, modified_verts, updated_seams_info)
     
@@ -71,21 +73,31 @@ def select_default(args, smpl_dir):
     garment.store_seamline_vertex_pairs(subdir=f'{args.design}-{args.body_set}')
 
 
+def run_parameterization(is_skirtified, use_darts):
+    cpp_program_path = "./garment-parameterization/build/optimize_set_with_seamlines"
+
+    optim_dress_arg = "1" if is_skirtified else "0"
+    use_darts_arg = "1" if use_darts else "0"
+
+    command = [cpp_program_path, optim_dress_arg, use_darts_arg]
+    try:
+        subprocess.run(command, check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running the C++ program: {e}")
+        print(f"Error output: {e.stderr}")
+    except FileNotFoundError:
+        print(f"The C++ program was not found at {cpp_program_path}")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--regenerate', '-R', action='store_true', dest='regenerate')
     parser.add_argument('--use_darts', action='store_true', dest='use_darts')
     parser.add_argument('--file_format', '-F', type=str, choices=['ply', 'obj', 'both'], default='ply')
     parser.add_argument('--design', '-D', type=str, default='default')
     parser.add_argument('--body_set', type=str, default="set2")
-    parser.add_argument('--os', type=str, default="linux")
+    parser.add_argument('--smpl_dir', type=str, default="/home/kristijan/data/smpl/models/")
     parser.add_argument('--standard_export', action='store_true', dest='standard_export')
     args = parser.parse_args()
-
-    if args.os == 'macos':
-        smpl_dir = '/Users/kristijanbartol/Documents/data/hood_data/aux_data/smpl/'
-    else:
-        smpl_dir = '/home/kristijan/data/smpl/models/'
 
     if os.path.exists('data/embedded/latest/'):
         rmtree('data/embedded/latest/')
@@ -95,10 +107,21 @@ if __name__ == '__main__':
     with open(f'config/designs/{args.design}.json', 'r') as json_file:
         design_dict = json.load(json_file)
 
+    is_skirtified = False
+
+    print('#1 Generating specified embedded design...')
     if not design_dict['flags']['skirtified']:
-        select_default(args, smpl_dir)
+        select_default(args)
     else:
         if design_dict['flags']['type'] == 'dress':
-            select_skirtified(args, smpl_dir)
+            select_skirtified(args)
+            is_skirtified = True
         else:
-            pass
+            print('If the skirtified flag is selected, the garment type should be (dress).')
+
+    print('#2 Running parameterization...')
+    run_parameterization(is_skirtified, args.use_darts)
+
+    print('#3 Visualize the optimized pattern...')
+    visualize_pattern(is_skirtified)
+    
--- visualize_pattern.py
+++ visualize_pattern.py
@@ -1,102 +1,8 @@
-import open3d as o3d
-import numpy as np
-import cv2
-import os
-
-GLOBAL_SCALE = 400
-
-
-class Mesh:
-    def __init__(self, ply_path, offset, is_front):
-        self.mesh = o3d.io.read_triangle_mesh(ply_path)
-        self.offset = offset
-        self.is_front = is_front
-
-
-def mesh_to_image(mesh, image_size=(800, 800)):
-    # Project the 3D mesh to 2D
-    points = np.asarray(mesh.vertices)
-    min_bounds = points.min(axis=0)
-    max_bounds = points.max(axis=0)
-    
-    points[:, :2] = (points[:, :2] - min_bounds[:2]) * GLOBAL_SCALE
-
-    # Create an empty image with alpha channel (transparent background)
-    image = np.zeros((image_size[1], image_size[0], 4), dtype=np.uint8)
-
-    triangles = np.asarray(mesh.triangles)
-    edge_color = (42, 42, 165, 255)
-    for tri in triangles:
-        pts = points[tri][:, :2].astype(int)
-        cv2.fillConvexPoly(image, pts, (0, 0, 0, 255))  # Black color for the mesh
-        cv2.polylines(image, [pts], isClosed=True, color=edge_color, thickness=1)  # Draw edges with brown color
-    
-    return image[::-1]
-
-
-def draw_mesh_on_canvas(canvas, mesh_image, offset, color):
-    center = int(canvas.shape[0] / 2), int(canvas.shape[1] / 2)
-    half_sizes = int(mesh_image.shape[0] / 2), int(mesh_image.shape[1] / 2)
-    x_offset, y_offset = offset
-    y1, y2 = center[1] + y_offset - half_sizes[1], center[1] + y_offset + half_sizes[1]
-    x1, x2 = center[0] + x_offset - half_sizes[0], center[0] + x_offset + half_sizes[0]
-
-    # Extract color channels if the image has an alpha channel
-    if mesh_image.shape[2] == 4:
-        img_rgb = mesh_image[..., :3]
-        img_alpha = mesh_image[..., 3]
-    else:
-        img_rgb = mesh_image
-        img_alpha = None
-
-    # Apply the color to the mesh while preserving transparency
-    colored_img = cv2.addWeighted(img_rgb, 0.5, np.full_like(img_rgb, color), 0.5, 0)
-    
-    if img_alpha is not None:
-        mask = img_alpha / 255.0
-        for c in range(0, 3):
-            canvas[y1:y2, x1:x2, c] = canvas[y1:y2, x1:x2, c] * (1 - mask) + colored_img[..., c] * mask
-        # Update the alpha channel of the canvas
-        canvas[y1:y2, x1:x2, 3] = np.maximum(canvas[y1:y2, x1:x2, 3], mesh_image[..., 3])
-    else:
-        canvas[y1:y2, x1:x2, :3] = colored_img
-
-
-def combine_meshes(meshes, canvas_size, img_size, front_color=(0, 255, 255), back_color=(0, 165, 255)):
-    # Create a blank canvas with alpha channel (transparent background)
-    canvas = np.zeros((canvas_size[1], canvas_size[0], 4), dtype=np.uint8)
-
-    for mesh in meshes:
-        mesh_image = mesh_to_image(mesh.mesh, image_size=img_size)
-        color = front_color if mesh.is_front else back_color
-        draw_mesh_on_canvas(canvas, mesh_image, mesh.offset, color)
-
-    return canvas
-
-
-def save_combined_image(output_path, combined_image):
-    cv2.imwrite(output_path, combined_image)
-
-
-
-PATTERN_DICT = {
-    'upper_front': [-20, 50],
-    'upper_back': [0, -300],
-    'sleeve_front_right': [-250, -220],
-    'sleeve_back_right': [-220, -330],
-    'sleeve_front_left': [300, -220],
-    'sleeve_back_left': [280, -330],
-    'lower_front_right': [-250, 220],
-    'lower_back_right': [-170, 280],
-    'lower_front_left': [220, 320],
-    'lower_back_left': [250, 200]
-}
-
-
-PATTERN_DICT_SKIRTIFIED = {
-    'upper_front': [-150, 100],
-    'upper_back': [150, 100]
-}
+from vis import (
+    PATTERN_DICT,
+    PATTERN_DICT_SKIRTIFIED,
+    visualize_pattern
+)
 
 
 DRESS = False
@@ -109,26 +15,4 @@ else:
 
 
 if __name__ == "__main__":
-    data_dir = f'data/embedded/latest/skintight/'
-
-    meshes_dict = {}
-    for _, dirs, _ in os.walk(data_dir):
-        for subdir in dirs:
-            for fname in os.listdir(os.path.join(data_dir, subdir)):
-                if 'optim' in fname:
-                    suffix = fname.split('.')[0][5:]
-                    pattern_fpath = os.path.join(data_dir, subdir, fname)
-                    is_front = True if subdir.split('_')[1] == 'front' else False
-                    if suffix not in meshes_dict:
-                        meshes_dict[suffix] = [Mesh(pattern_fpath, pattern_dict[subdir], is_front)]
-                    else:
-                        meshes_dict[suffix].append(Mesh(pattern_fpath, pattern_dict[subdir], is_front))
-    for suffix in meshes_dict:
-        meshes_dict[suffix].sort(key=lambda mesh: mesh.is_front)
-
-        canvas_size = (1200, 1200)
-        img_size = (450, 450)
-        combined_image = combine_meshes(meshes_dict[suffix], canvas_size, img_size)
-
-        output_path = os.path.join(data_dir, f'sewing_pattern{suffix}.png')
-        save_combined_image(output_path, combined_image)
+    visualize_pattern(DRESS)
